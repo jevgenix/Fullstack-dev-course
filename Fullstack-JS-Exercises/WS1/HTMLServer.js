c705695db9978a82fd77313b2c3bf4581e183be2
jevgenix <71660541+jevgenix@users.noreply.github.com>
2022-02-08 14:53:41 +0200

    sss

--- Fullstack-JS-Exercises/WS1/HTMLServer.js
+++ Fullstack-JS-Exercises/WS1/HTMLServer.js
@@ -1,8 +1,10 @@
-var http = require('http');
+var http = require("http");
 // create server object
-http.createServer (function (resrequest, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
+http
+  .createServer(function (resrequest, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
     response.write("<h1> Hello user </h1>");
     response.write("<h2> Just testing </h2>");
     response.end("<h1> Bye! </h1>");
-}).listen(3031);
+  })
+  .listen(3031);
